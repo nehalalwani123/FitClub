8c95ff4a0199ea150c0a5aa69aa99eef01f6bd84
nehalalwani123 <111523029+nehalalwani123@users.noreply.github.com>
2023-08-25 17:08:36 +0530

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,153 @@
+import React from "react";
+import { motion } from "framer-motion";
+import CountUp from "react-countup";
+//import componest
+import Header from "../Header/Header";
+
+//import css
+import "./Hero.css";
+
+//import static file
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
+
+const theBestVariants = {
+  start: {
+    x: 220,
+  },
+  end: {
+    x: 7,
+    transition: {
+      type: "tween",
+      duration: 3,
+    },
+  },
+};
+
+const heartRateVariants = {
+  start: {
+    right: "-1rem",
+  },
+  end: {
+    // x: "-50px",
+    right: "4rem",
+    transition: {
+      type: "tween",
+      duration: 3,
+    },
+  },
+};
+const caloriesVariants = {
+  start: {
+    x: "-1rem",
+  },
+  end: {
+    // x: "-50px",
+    x: "5rem",
+    transition: {
+      type: "tween",
+      duration: 3,
+    },
+  },
+};
+
+const Hero = () => {
+  const transition = { type: "spring", duration: 3 };
+  return (
+    <div className="hero" id="home">
+      <div className="blur hero-blur"></div>
+      <div className="left-h">
+        <Header />
+        {/* the best add */}
+        <div className="the-best-ad">
+          <motion.div
+            variants={theBestVariants}
+            initial="start"
+            // animate="end"
+            whileInView="end"
+          ></motion.div>
+          <span>the best fitness club in town</span>
+        </div>
+
+        {/* Hero headding  */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape</span>
+            <span>your</span>
+          </div>
+          <div>
+            <span>ideal body</span>
+          </div>
+          <div>
+            <span>
+              in here we will help you to shape and build your ideadl body and
+              live up your live to fulllest
+            </span>
+          </div>
+        </div>
+
+        {/* figures */}
+        <div className="figure">
+          <div>
+            <span>
+              <CountUp end={140} start={100} delay={2} prefix="+" />
+            </span>
+            <span>expert coaching</span>
+          </div>
+          <div>
+            <span>
+              <CountUp end={987} start={500} delay={2} prefix="+" />
+            </span>
+            <span>member joined</span>
+          </div>
+          <div>
+            <span>
+              <CountUp end={50} start={20} delay={2} prefix="+" />
+            </span>
+            <span>fitness program </span>
+          </div>
+        </div>
+
+        {/* herro button */}
+        <div className="hero-button">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
+      </div>
+      <div className="right-h">
+        <button className="btn"> Join Now</button>
+        <motion.div
+          className="heart-rate"
+          variants={heartRateVariants}
+          initial="start"
+          whileInView="end"
+        >
+          <img src={Heart} alt="" />
+          <span>Heart rate</span>
+          <span>116 bpm</span>
+        </motion.div>
+        {/* hero imgae */}
+        <img src={hero_image} alt="" className="hero-image" />
+        <img src={hero_image_back} alt="" className="hero-image_back" />
+        {/* hero calories */}
+        <motion.div
+          className="calories"
+          variants={caloriesVariants}
+          initial="start"
+          // animate='end'
+          whileInView="end"
+        >
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories burn</span>
+            <span>220 cal</span>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import "./Testimonial.css";
+import { testimonialsData } from "../../data/testimonialsData";
+import leftArrow from "../../assets/leftArrow.png";
+import rightArrow from "../../assets/rightArrow.png";
+
+const messageVariants = {
+  hidden: {
+    x: -20,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: "easeOut",
+    },
+  },
+  exit: {
+    x: "-100vw",
+    opacity: 0,
+  },
+};
+const messageVariants2 = {
+  hidden: {
+    x: -20,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: "easeOut",
+    },
+  },
+  exit: {
+    x: "-100vw",
+    opacity: 0,
+  },
+};
+
+const imageVariants = {
+  hidden: {
+    x: 20,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "tween",
+      duration: 1,
+      ease: "easeOut",
+    },
+  },
+};
+
+const Testimonial = () => {
+  const [selected, setSelected] = useState(0);
+  const tLength = testimonialsData.length;
+
+  return (
+    <div className="Testimonials" id="testimonials">
+      <div className="left-t">
+        <span>Testimonials</span>
+        <span className="stroke-text">what they</span>
+        <span>say about us</span>
+        <motion.span
+          key={selected}
+          variants={messageVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        >
+          {testimonialsData[selected].review}
+        </motion.span>
+        <motion.span>
+          <span style={{ color: "var(--orange)" }}>
+            {testimonialsData[selected].name}
+          </span>{" "}
+          - {testimonialsData[selected].status}
+        </motion.span>
+      </div>
+      <div className="right-t">
+        <div></div>
+        <div></div>
+        <motion.img
+          variants={imageVariants}
+          initial="hidden"
+          animate="visible"
+          key={selected}
+          src={testimonialsData[selected].image}
+          alt=""
+        />
+
+        <div className="arrows">
+          <img
+            onClick={() => {
+              selected === 0
+                ? setSelected(tLength - 1)
+                : setSelected((prev) => prev - 1);
+            }}
+            src={leftArrow}
+            alt=""
+          />
+          <img
+            onClick={() => {
+              selected === tLength - 1
+                ? setSelected(0)
+                : setSelected((prev) => prev + 1);
+            }}
+            src={rightArrow}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
